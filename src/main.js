e85b32eed34a41ec4d459020f8fa8e1e691a78be
Amaan Hassim <amaanhassim@gmail.com>
2023-11-26 18:46:53 +0000

    added global css and vuetify ui library

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
+import "./assets/global.css";
 
-createApp(App).mount("#app");
+//vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(vuetify).mount("#app");
